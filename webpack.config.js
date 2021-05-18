607cd32fd5396127d02c9a16b5277c26c074c923
dianlight <lucio.tarantino@gmail.com>
2021-05-18 08:44:31 +0200

    fix: d.ts and clean on bluild

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 var path = require("path");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
+
 module.exports = {
     mode: 'production',
     entry: {
-        gcodetogeometry: "./src/gcodetogeometry.js"
+        gcodetogeometry: "./src/gcodetogeometry.ts"
     },
     output: {
         path: path.join(__dirname, "dist"),
@@ -18,4 +20,26 @@ module.exports = {
         compress: true,
         port: 9000,
     },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+        rules: [
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+            },
+            {
+                test: /\.m?js/,
+                resolve: {
+                    fullySpecified: false
+                }
+            }
+        ]
+    },
+    plugins: [
+        new NodePolyfillPlugin(),
+    ]
 };
